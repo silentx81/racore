b10d872ace691e2529daad28dee059099af69ad7
Raffael Wyss <raffael.wyss@gmail.com>
2012-11-29 22:43:07 +0100

    Main-Class

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User:        Raffael Wyss
+ * Date:        29.11.12
+ * Time:        21:43
+ * Description: Startdatei
+ */
+
+/**
+ * Main-Requirements
+ */
+require_once 'layer/core/Main.class.php';
+
+/**
+ * Start the Main-Process
+ */
+$Main = new Main();
---
+++ public_html/layer/core/Main.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User:        Raffael Wyss
+ * Date:        29.11.12
+ * Time:        21:57
+ * Description:
+ */
+class Main
+{
+    public function __construct()
+    {
+        echo "hallo <hr>";
+    }
+}
